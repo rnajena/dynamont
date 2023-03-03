decb3459212b55a16d0505591541d8c48df5e582
Jannes Spangenberg <janspang@gmx.de>
2023-03-03 16:25:34 +0100

    Add CLI
    - added basic functions to parse command line arguments
    - added doc strings to functions

--- src/segment.cpp
+++ src/segment.cpp
@@ -8,12 +8,22 @@
 #include <cmath> // exp
 #include <limits> // for inifinity
 #include <math.h> // log1p
+#include <filesystem> // reading files in directory (fast5)
+#include <vector>
 
 using namespace std;
+namespace fs = filesystem;
 
-//Asserts floating point compatibility at compile time
+// Asserts floating point compatibility at compile time
+// necessary for INFINITY usage
 static_assert(numeric_limits<float>::is_iec559, "IEEE 754 required");
 
+/**
+ * Read the normal distribution parameters from a given TSV file
+ *
+ * @param file path to the TSV file containing the parameters
+ * @param return map containing kmers as keys and (mean, stdev) tuples as values
+ */
 map<string,tuple<float, float>> readKmerModel(string file) {
     ifstream inputFile;
     inputFile.open(file);
@@ -34,19 +44,45 @@ map<string,tuple<float, float>> readKmerModel(string file) {
     return model;
 }
 
-float normal_pdf(float x, float m, float s)
-{
+// https://stackoverflow.com/questions/10847007/using-the-gaussian-probability-density-function-in-c
+/**
+ * Calculate probabilty value for a given x, mean and standard deviation of a normal distribution
+ *
+ * @param x value
+ * @param m mean
+ * @param s standard deviation 
+ */
+float normal_pdf(float x, float m, float s) {
     static const float inv_sqrt_2pi = 0.3989422804014327;
     float a = (x - m) / s;
 
     return inv_sqrt_2pi / s * exp(-0.5f * a * a);
 }
 
-float score5mer(float signalpoint, string kmer, map<string,tuple<float, float>>* model) {
+/**
+ * Return probability density for a given value and a given normal distribution
+ *
+ * @param x point to calculate probability density
+ * @param kmer key for the model kmer:(mean, stdev) map
+ * @param model map containing kmers as keys and (mean, stdev) tuples as values
+ * @return probability density value for x in the given normal distribution
+ */
+float score5mer(float x, string kmer, map<string,tuple<float, float>>* model) {
     tuple<float, float> kmerModel = model->find(kmer)->second;
-    return normal_pdf(signalpoint, get<0>(kmerModel), get<1>(kmerModel));
+    return normal_pdf(x, get<0>(kmerModel), get<1>(kmerModel));
 }
 
+/**
+ * Calculate forward matrices
+ *
+ * @param signal ONT raw signal with pA values
+ * @param seq nucleotide sequence represented by the ONT signal
+ * @param MM matrix containing values for segment borders 
+ * @param MC matrix containing values for extending segment
+ * @param T length of the ONT raw signal
+ * @param N length of nucleotide sequence
+ * @param model map containing kmers as keys and (mean, stdev) tuples as values
+ */
 void forward(float* signal, string* seq, float* MM, float* MC, int T, int N, map<string,tuple<float, float>>* model){
     // int T = sizeof(signal)/sizeof(*signal);
     // int N = seq->size();
@@ -73,6 +109,13 @@ void forward(float* signal, string* seq, float* MM, float* MC, int T, int N, map
     }
 }
 
+/**
+ * Calculate addition of a+b in log space as efficiently as possible
+ *
+ * @param a first value
+ * @param b second value
+ * @returns log(exp(a) + exp(b))
+ */
 float logplus(float a, float b) {
     // safety check, should not occure
     // if(a==b && isinf(a) && isinf(b)) {
@@ -84,6 +127,17 @@ float logplus(float a, float b) {
     return b + log1p(a-b);
 }
 
+/**
+ * Calculate forward matrices using logarithmic values
+ *
+ * @param signal ONT raw signal with pA values
+ * @param seq nucleotide sequence represented by the ONT signal
+ * @param MM matrix containing values for segment borders 
+ * @param MC matrix containing values for extending segment
+ * @param T length of the ONT raw signal
+ * @param N length of nucleotide sequence
+ * @param model map containing kmers as keys and (mean, stdev) tuples as values
+ */
 void logForward(float* signal, string* seq, float* MM, float* MC, int T, int N, map<string,tuple<float, float>>* model){
     // int T = sizeof(signal)/sizeof(*signal);
     // int N = seq->size();
@@ -110,28 +164,68 @@ void logForward(float* signal, string* seq, float* MM, float* MC, int T, int N, 
     }
 }
 
-int main() {
-    // float sig[10] = {1.0,2.0,3.0,2.0,3.0,8.0,7.0,9.0,7.0,9.0};
-    // int seq[2] = {0,1};
-    // int T = 5 + 1;
-    // int N = 2 + 1;
-    // forward(MM, MC, T, N);
-    // for(int i=0;i<T;i++){
-    //     for(int j=0;j<N;j++){
-    //         cout<<MM[i*N+j]<<' ';
-    //     }
-    //     cout<<endl;
-    // }
-    // map<string,tuple<float, float>> model = readKmerModel();
-    // cout<<get<0>(model.find("AAAAA")->second)<<"     "<<get<1>(models.find("AAAAA")->second);
-    // map<string,tuple<float, float>> :: iterator i;
-    // cout<<"Keys"<<"  &  "<<"Value"<<endl;
-    // for (i = models.begin(); i!= models.end(); i++) {
-    //     cout<<(*i).first<<"    "<<get<0>((*i).second)<<"    "<<get<1>((*i).second)<<"\n";
-    // }
-    // int* a;
-    // int* b;
-    // tuple<int*, int*> t = test();
-    // a = get<0>(t);
-    // cout<<a[0];
+// https://stackoverflow.com/questions/11140483/how-to-get-list-of-files-with-a-specific-extension-in-a-given-folder
+/**
+ * Collect all fast5 files in the given directory
+ *
+ * @param directory path to the parent directory containing files with the given extension
+ * @param ext collecting files containing this extension
+ * @return list of all files with the given extension in the directory
+ */
+vector<fs::path> extractFiles(fs::path const & directory, string const & ext = ".fast5") {
+    vector<fs::path> paths;
+    if (fs::exists(directory) && fs::is_directory(directory)) {
+        for (auto const & entry : fs::recursive_directory_iterator(directory)) {
+            if (fs::is_regular_file(entry) && entry.path().extension() == ext)
+                paths.emplace_back(entry.path().filename());
+        }
+    }
+    // else throw exception?
+    return paths;
+}  
+
+// https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
+/**
+ * Return the value of the command line argument
+ *
+ * @param begin command line arguments
+ * @param end end of command line arguments
+ * @param option command line argument to search for
+ * @return value of the given argument if there is one
+ */
+char* getCmdOption(char ** begin, char ** end, const string & option) {
+
+    char ** itr = find(begin, end, option);
+    if (itr != end && ++itr != end)
+    {
+        return *itr;
+    }
+    return 0;
+}
+
+// https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
+/**
+ * Checks if command line argument exists
+ * 
+ * @param begin command line arguments
+ * @param end end of command line arguments
+ * @param option command line argument to search for
+ * @return True or False
+ */
+bool cmdOptionExists(char** begin, char** end, const string& option) {
+    return find(begin, end, option) != end;
+}
+
+int main(int argc, char* argv[]) {
+
+    if(cmdOptionExists(argv, argv+argc, "-h")) {
+        // TODO print help message
+    }
+
+    // input fast5 parent directory
+    char * fast5_directory = getCmdOption(argv, argv + argc, "-f5");
+
+    if (fast5_directory) {
+        vector<fs::path> fast5s = extractFiles(fast5_directory);
+    }
 }
