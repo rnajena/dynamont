0b405b1dfcd24c98e42a8974a77f0027fb652137
Jannes Spangenberg <janspang@gmx.de>
2023-04-18 11:59:09 +0200

    Initial Commit

---
+++ src/segment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# author: Jannes Spangenberg
+# e-mail: jannes.spangenberg@uni-jena.de
+# github: https://github.com/JannesSP
+# website: https://jannessp.github.io
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
+import os
+import h5py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+
+def parse() -> Namespace:
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    #TODO insert arguments
+    return parser.parse_args()
+
+def main() -> None:
+    args = parse()
+    #TODO do stuff
+
+if __name__ == '__main__':
+    main()
