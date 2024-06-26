4d0821021fd4363950624dd4ca34bb42b3586a1e
Jannes Spangenberg <janspang@gmx.de>
2024-06-26 15:32:40 +0200

    extract normalised models

---
+++ data/calc_norm_models.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# author: Jannes Spangenberg
+# e-mail: jannes.spangenberg@uni-jena.de
+# github: https://github.com/JannesSP
+# website: https://jannessp.github.io
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
+import numpy as np
+from read5 import read
+from itertools import product
+from scipy.stats import median_abs_deviation as mad
+
+def parse() -> Namespace:
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('raw', type=str, help='Raw ONT training data')
+    parser.add_argument('dynamont', type=str, help='Dynamont segmentation')
+    parser.add_argument('outfile', type=str, help='Outfile with models in 3\' -> 5\' orientation')
+    return parser.parse_args()
+
+def readDynamont(file : str) -> dict:
+    res = {}
+    print(f"Reading {file}")
+    with open(file, 'r') as r:
+        r.readline()
+        for line in r:
+            readid,start,end,basepos,base,motif,state = line.strip().split(',')
+            # skip short motifs
+            if len(motif) < 5:
+                continue
+            if not readid in res:
+                res[readid] = []
+            res[readid].append([int(start), int(end), motif.replace('U', 'T')])
+    return res
+
+def extractSegments(normSignal : np.ndarray, segments : list, models : dict) -> dict:
+    for segment in segments:
+        start, end, motif = segment
+        # skip short segments
+        if end-start < 6:
+            continue
+        models[motif].extend(normSignal[start:end])
+    return models
+
+def writeModels(models : dict, outfile : str) -> None:
+    print(f"Writing {len(models)} models to {outfile}")
+    with open(outfile, 'w') as w:
+        w.write('kmer\tlevel_mean\tlevel_stdv\n')
+        for motif in [''.join(motif) for motif in list(product('ACGT', repeat=5))]:
+            w.write(f'{motif}\t{np.median(models[motif[::-1]])}\t{mad(models[motif[::-1]])}\n')
+
+def main() -> None:
+    args = parse()
+    r5 = read(args.raw)
+    dynamont = readDynamont(args.dynamont)
+
+    models = {''.join(motif) : [] for motif in list(product('ACGT', repeat=5))}
+    print("Extracting segments")
+    for rnum, readid in enumerate(dynamont):
+        if not readid in r5.getReads():
+            continue
+        if (rnum+1)%1000==0:
+            print(f"Extracting from read {rnum+1}", end='\r')
+        if (rnum+1)%10000==0:
+            writeModels(models, args.outfile + f"_{rnum+1}")
+        models = extractSegments(r5.getZNormSignal(readid), dynamont[readid], models)
+    writeModels(models, args.outfile)
+
+if __name__ == '__main__':
+    main()
