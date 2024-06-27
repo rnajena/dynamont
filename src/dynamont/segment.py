9512e52ad2d7fee6d99753cac653eccc132af361
Jannes Spangenberg <janspang@gmx.de>
2024-06-27 16:28:51 +0200

    make segmentation asynchronous

--- src/dynamont/segment.py
+++ src/dynamont/segment.py
@@ -4,10 +4,12 @@
 # github: https://github.com/JannesSP
 # website: https://jannessp.github.io
 
+# TODO make this asynchroneous
+
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 from os.path import exists, join, dirname
 from os import makedirs, name
-from FileIO import getFiles, loadFastx, genSegmentTable
+from FileIO import getFiles, loadFastx, openCPPScript, feedSegmentationAsynchronous
 from read5 import read
 import multiprocessing as mp
 
@@ -24,14 +26,27 @@ def parse() -> Namespace:
     parser.add_argument('--mode', choices=['basic', 'indel'], required=True)
     return parser.parse_args()
 
+def listener(q : mp.Queue, outfile : str):
+    '''listens for messages on the q, writes to file. '''
+    with open(outfile, 'w') as f:
+        f.write('readid,start,end,basepos,base,motif,state\n')
+        i = 0
+        while 1:
+            m = q.get()
+            i+=1
+            if i%1000==0:
+                print(f"Segmented {i} reads", end='\r')
+            if m == 'kill':
+                break
+            f.write(m)
+            f.flush()
+
 # def segment(rawdatapath : str, fastxpath : str, polya : dict, batch_size : int, mode : str, outfile : str, modelpath : str) -> None:
 def segment(rawdatapath : str, fastxpath : str, batch_size : int, mode : str, outfile : str, modelpath : str) -> None:
     files = getFiles(rawdatapath, True)
     print(f'ONT Files: {len(files)}')
     basecalls = loadFastx(fastxpath)
     print(f'{len(basecalls)} reads found')
-
-    out = open(outfile, 'w')
     
     if mode == 'indel':
         CPP_SCRIPT = join(dirname(__file__), 'segmentation_indel')
@@ -41,49 +56,29 @@ def segment(rawdatapath : str, fastxpath : str, batch_size : int, mode : str, ou
         CPP_SCRIPT = join(dirname(__file__), 'segmentation_basic')
         if name == 'nt': # check for windows
             CPP_SCRIPT+='.exe'
+    
     i = 0
+    pool = mp.Pool(batch_size)
+    pipes = [openCPPScript(CPP_SCRIPT) for _ in range(batch_size - 1)]
+    queue = mp.Manager().Queue()
+    pool.apply_async(listener, (queue, outfile))
 
-    out.write('readid,start,end,basepos,base,motif,state\n')
-
-    with mp.Pool(batch_size) as p:
-        for file in files:
-
-            r5 = read(file)
-            mp_items = []
-
-            for readid in r5:
-                if not readid in basecalls: # or (readid not in polyAIndex)
-                    continue
-
-                # skip reads with undetermined transcript start, only take really good reads for training
-                # if not readid in polya or polya[readid][1] == -1 or polya[readid][1] - polya[readid][0] < 30:
-                #     signal = r5.getPolyAStandardizedSignal(readid, polya[readid][0], polya[readid][1])[polya[readid][1]:]
-                # else:
-
-                signal = r5.getpASignal(readid)
-                
-                # if polya[readid][1] - polya[readid][0] < 30:
-                #     continue
-
-                # signal = r5.getPolyAStandardizedSignal(readid, polya[readid][0], polya[readid][1])[polya[readid][1]:]
-                mp_items.append([signal, basecalls[readid][::-1], CPP_SCRIPT, readid, modelpath])
-
-                if len(mp_items)%batch_size == 0:
-                    for result in p.starmap(genSegmentTable, mp_items):
-                        out.write(result)
-                        i += 1
-                    print(f"Segmented {i} reads", end='\r')
-                    # initialize new batch
-                    mp_items = []
-
-            # last unfilled batch
-            if len(mp_items):
-                for result in p.starmap(genSegmentTable, mp_items):
-                    out.write(result)
-                    i += 1
-                print(f"Segmented {i} reads")
-                # initialize new batch
-                mp_items = []
+    jobs = []
+    for file in files:
+        r5 = read(file)
+        for readid in r5:
+            if not readid in basecalls: # or (readid not in polyAIndex)
+                continue
+            job = pool.apply_async(feedSegmentationAsynchronous, (r5.getpASignal(readid, "mean"), basecalls[readid][::-1], readid, pipes[i%len(pipes)], queue))
+            jobs.append(job)
+            i+=1
+            
+    for job in jobs:
+        job.get()
+    
+    queue.put("kill")
+    pool.close()
+    pool.join()
 
 def main() -> None:
     args = parse()
