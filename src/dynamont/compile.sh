be5b5d9ced6d9fa9523ccfbd1a1ac23552cd903f
Jannes Spangenberg <janspang@gmx.de>
2024-09-10 16:41:34 +0200

    typo

--- src/dynamont/compile.sh
+++ src/dynamont/compile.sh
@@ -1,3 +1,3 @@
 g++ -g -O3 -Wall -o segmentation_3d_sparsed segmentation_3d_sparsed.cpp utils.cpp
 g++ -g -O3 -Wall -o segmentation_basic_sparsed segmentation_basic_sparsed.cpp utils.cpp
-g++ -g -O3 -Wall -o segmentation_indel segmentation_indel_.cpp utils.cpp
+g++ -g -O3 -Wall -o segmentation_indel segmentation_indel.cpp utils.cpp
