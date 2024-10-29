6f0d63333984adbeb9aec64e1580c81b44939fe8
Jannes Spangenberg <janspang@gmx.de>
2024-10-29 15:24:50 +0100

    update date

--- data/README.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# imports pandas, itertools and os
-python3 extended_model.py
-
-wget https://github.com/hasindu2008/f5c/blob/master/test/rna004-models/rna004.nucleotide.5mer.model
-
-wget https://raw.githubusercontent.com/nanoporetech/kmer_models/master/rna004/9mer_levels_v1.txt
