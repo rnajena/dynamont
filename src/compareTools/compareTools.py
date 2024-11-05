6d56dd23f0cb7c062eac41e0f92578067a519595
Jannes Spangenberg <janspang@gmx.de>
2024-11-05 17:21:26 +0100

    add docstrings and multiprocessing

--- src/compareTools/compareTools.py
+++ src/compareTools/compareTools.py
@@ -12,8 +12,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import defaultdict
+import multiprocessing as mp
 
 def parse() -> Namespace:
+    """
+    Parse command line arguments for tool comparison.
+
+    Returns:
+        Namespace: Containing the specified command line arguments
+    """
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter
     )
@@ -23,20 +30,55 @@ def parse() -> Namespace:
     parser.add_argument("--f5cresquiggle", type=str, required=True, metavar="TSV", help="f5c resquiggle segmentation output in TSV format")
     parser.add_argument("--f5ceventalign", type=str, required=True, metavar="TSV", help="f5c eventalign segmentation output in TSV format\nSummary file must exists in the same path with extension .sum")
     parser.add_argument("--tombo", type=str, required=True, metavar="PATH", help="Parent directory of single fast5s processed by Tombo")
+    parser.add_argument("--basecalls", type=str, required=True, metavar="BAM", help="Basecalls of ONT training data as .bam file")
+    # parser.add_argument("--pod5", type=str, required=True, metavar="POD5", help="raw signals")
     parser.add_argument("--output", type=str, required=True, metavar="CSV", help="Output CSV containing pandas data frame")
     return parser.parse_args()
 
 def readDynamont(file: str) -> dict:
+    """
+    Reads a CSV file containing Dynamont segmentation output and extracts
+    signal IDs along with their corresponding start and end positions.
+
+    Parameters
+    ----------
+    file : str
+        Path to the CSV file containing Dynamont segmentation output.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each signal ID to a set of start and end positions.
+    """
+    print("Reading Dynamont output from " + file)
     readMap = defaultdict(set)
     with open(file, 'r') as f:
         next(f) # skip header
         # readid,signalid,start,end,basepos,base,motif,state,posterior_probability,polish
         for line in f:
-            _, signalid, start, end, *_ = line.strip().split(',')
+            try:
+                _, signalid, start, end, *_ = line.strip().split(',')
+            except ValueError: # empty line in file
+                pass
             readMap[signalid].update([int(start), int(end)])
     return readMap
 
 def readDorado(file: str) -> dict:
+    """
+    Reads a TSV file containing Dorado segmentation output and extracts
+    signal IDs along with their corresponding start and end positions.
+
+    Parameters
+    ----------
+    file : str
+        Path to the TSV file containing Dorado segmentation output.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each signal ID to a set of start and end positions.
+    """
+    print("Reading Dorado output from " + file)
     readMap = defaultdict(set)
     with open(file, 'r') as f:
         next(f) # skip header
@@ -47,13 +89,30 @@ def readDorado(file: str) -> dict:
     return readMap
 
 def readF5CEventalign(file: str, summary : str) -> dict:
+    """
+    Reads a TSV file containing f5c eventalign segmentation output and extracts
+    read IDs along with their corresponding start and end positions.
+
+    Parameters
+    ----------
+    file : str
+        Path to the TSV file containing f5c eventalign segmentation output.
+    summary : str
+        Path to the .sum file containing the read names corresponding to the read indices in the segmentation output.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each read ID to a set of start and end positions.
+    """
+    print("Reading f5c eventalign output from " + file)
     readIdMap = {}
     with open(summary, 'r') as f:
         next(f) # skip header
         # read_index    read_name   fast5_path  model_name  strand  num_events  num_steps   num_skips   num_stays   total_duration  shift   scale   drift   var
         for line in f:
-            readid, read_name, *rest = line.strip().split('\t')
-            readIdMap[readid] = read_name
+            readid, readName, *rest = line.strip().split('\t')
+            readIdMap[readid] = readName
 
     readMap = defaultdict(set)
     with open(file, 'r') as f:
@@ -65,97 +124,276 @@ def readF5CEventalign(file: str, summary : str) -> dict:
     return readMap
 
 def readF5CResquiggle(file: str) -> dict:
+    """
+    Parses a TSV file to extract read IDs along with their corresponding start and end positions.
+
+    Parameters
+    ----------
+    file : str
+        Path to the TSV file containing resquiggle segmentation output.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each read ID to a set of start and end positions.
+    """
+    print("Reading f5c resquiggle output from " + file)
     readMap = defaultdict(set)
     with open(file, 'r') as f:
         next(f) # skip header
         # contig    position    reference_kmer  read_index  strand  event_index event_level_mean    event_stdv  event_length    model_kmer  model_mean  model_stdv  standardized_level  start_idx   end_idx
         for line in f:
+            # print(line)
             readid, _, start, end = line.strip().split('\t')
-            readMap[readid].update([int(start), int(end)])
+            try:
+                readMap[readid].update([int(start), int(end)])
+            except ValueError: # no segmentation for that position, e.g. 00b77311-c22c-472e-ad39-59f5f6aea4ec    467     .       .
+                pass
     return readMap
 
 def getFast5s(directory: str) -> list:
-    fast5_files = []
+    """
+    Recursively walks through a directory and its subdirectories to find all FAST5 files.
+
+    Parameters
+    ----------
+    directory : str
+        The root directory to start searching from.
+
+    Returns
+    -------
+    list
+        A list of paths to all FAST5 files found in the directory and its subdirectories.
+    """
+    print("Reading FAST5 files from " + directory)
+    fast5Files = []
     for root, _, files in os.walk(directory):
+        # check if root is absolute path
+        if not root.startswith("/"):
+            root = os.path.join(os.getcwd(), root)
         for file in files:
             if file.endswith('.fast5'):
-                fast5_files.append(os.path.join(root, file))
-    return fast5_files
+                filepath = os.path.join(root, file)
+                if not os.path.exists(filepath):
+                    print("File not found, path may be broken!: " + filepath)
+                    exit(1)
+                fast5Files.append(filepath)
+    return fast5Files
 
 def readTombo(directory: str) -> dict:
+    """
+    Recursively walks through a directory and its subdirectories to find all FAST5 files, then parses the 'RawGenomeCorrected_000/BaseCalled_template/Events' group in each FAST5 file to extract read IDs along with their corresponding start positions.
+
+    Parameters
+    ----------
+    directory : str
+        The root directory to start searching from.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each read ID to a set of start positions.
+    """
+    print("Reading tombo output from " + directory)
     fast5s = getFast5s(directory)
     readMap = defaultdict(set)
     for fast5 in fast5s:
         with h5py.File(fast5, 'r') as h5:
             readid = os.path.basename(fast5).split('.')[0]
-            events : np.ndarray = h5['Analyses/RawGenomeCorrected_000/BaseCalled_template/Events'][:]
+            try:
+                if h5['Analyses/RawGenomeCorrected_000/BaseCalled_template'].attrs['status'] == 'Alignment not produced':
+                    continue
+                events : np.ndarray = h5['Analyses/RawGenomeCorrected_000/BaseCalled_template/Events'][:]
+            except KeyError:
+                continue
             readMap[readid].update(events['start'].tolist())
     return readMap
 
 def readChangepoints(file : str) -> dict:
+    """
+    Reads a HDF5 file containing changepoints and extracts read IDs along with their corresponding changepoints.
+
+    Parameters
+    ----------
+    file : str
+        Path to the HDF5 file containing changepoints.
+
+    Returns
+    -------
+    dict
+        A dictionary mapping each read ID to a set of changepoints.
+    """
+    print("Reading changepoints from " + file)
     readMap = defaultdict(set)
     with h5py.File(file, 'r') as h5:
         for readid in h5.keys():
-            changepoints = h5[readid][:]
+            # print(h5[readid].keys())
+            changepoints = h5[readid + "/waveletEdge"][:]
             readMap[readid].update(changepoints)
     return readMap
 
 def toNumpy(readMap: dict) -> None:
+    """
+    Converts all values in the given readMap from a set to a sorted numpy array.
+
+    Parameters
+    ----------
+    readMap : dict
+        A dictionary mapping read IDs to a set of positions.
+    """
     for readid in readMap:
         readMap[readid] = np.array(list(readMap[readid]))
         readMap[readid].sort()
 
-def evaluate(groundTruth: dict, prediction: dict, maxDistance : int) -> dict:
+def evaluate(groundTruth: np.ndarray, prediction: np.ndarray, maxDistance : int) -> int:
+    """
+    Evaluates the prediction of a tool by counting how many of the ground truth
+    changepoints are found within a given maxDistance.
+
+    Parameters
+    ----------
+    groundTruth : np.ndarray
+        1D array of ground truth changepoint positions.
+    prediction : np.ndarray
+        1D array of predicted changepoint positions.
+    maxDistance : int
+        The maximum distance in which a predicted changepoint is considered
+        to be a true positive.
+
+    Returns
+    -------
+    int
+        The number of true positives.
+    """
     found = 0
-    for gt_value in groundTruth:
-        min_distance = min(np.abs(gt_value - prediction))
-        found += min_distance < maxDistance
+    for gtVal in groundTruth:
+        minDist = min(np.abs(gtVal - prediction))
+        found += minDist < maxDistance
     return found
 
 def plot(df: pd.DataFrame, outfile : str) -> None:
+    """
+    Plots a bar plot of the found edges for each tool at each maxDistance.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A DataFrame containing the found edges for each tool at each maxDistance.
+    outfile : str
+        The path to save the plot to.
+    """
     sns.set_theme()
     sns.barplot(df, x='maxDistance', y='foundEdges', hue='tool', log_scale=True)
     plt.savefig(outfile)
 
+def generateControl(bamFile : str) -> tuple:
+    """
+    Generates a control set for a given bam file.
+
+    For each read in the bam file, two types of control sets are generated:
+    - A set of randomly drawn borders without replacement.
+    - A set of equidistant borders.
+
+    The start and end positions of each read are stored in a dictionary for later use.
+
+    Returns
+    -------
+    tuple
+        A tuple containing two dictionaries: randomBorders and uniformBorders.
+        Each dictionary contains a list of borders for each read in the bam file.
+    """
+    print("Generating control set from " + bamFile)
+    import pysam
+    basecalledRegions = {}
+    with pysam.AlignmentFile(bamFile, "rb", check_sq=False) as bamfile:
+        for read in bamfile.fetch(until_eof=True):
+            readid = read.get_tag("pi") if read.has_tag("pi") else read.query_name
+            sp = read.get_tag("sp") if read.has_tag("sp") else 0 # if split read get start offset of the signal
+            ts = read.get_tag("ts") # ts:i: 	the number of samples trimmed from the start of the signal
+            ns = read.get_tag("ns") # ns:i: 	the number of samples in the signal prior to trimming
+            nts = len(read.query_sequence)
+            
+            if readid not in basecalledRegions:
+                basecalledRegions[readid] = []
+
+            # store information: number of nucleotides, start position in signal, end position in signal
+            basecalledRegions[readid].append([nts, sp+ts, sp+ns])
+
+    randomBorders = {} # randomly drawn borders without replacement
+    uniformBorders = {} # equidistant borders
+    for readid in basecalledRegions:
+        regions = np.array([])
+        uniformBorders[readid] = []
+        nts = 0
+        for region in basecalledRegions[readid]:
+            nts += region[0]
+            regions = np.append(regions, np.arange(region[1], region[2], 1))
+            uniformBorders[readid].extend(np.linspace(region[1], region[2] - 1, region[0]).tolist())
+
+        randomBorders[readid] = np.random.choice(regions, size=nts, replace=False)
+        uniformBorders[readid] = np.round(np.array(uniformBorders[readid])).astype(int)
+        
+    return randomBorders, uniformBorders
+
 def main() -> None:
     args = parse()
 
+    pool = mp.Pool(7)
+
     if not os.path.exists(args.output):
-        groundTruths = readChangepoints(args.changepoints)
+        gtReturn = pool.apply_async(readChangepoints, args=(args.changepoints,))
+        dynamontReturn = pool.apply_async(readDynamont, args=(args.dynamont,))
+        f5ceventalignReturn = pool.apply_async(readF5CEventalign, args=(args.f5ceventalign, os.path.splitext(args.f5ceventalign)[0] + '.sum'))
+        f5cresquiggleReturn = pool.apply_async(readF5CResquiggle, args=(args.f5cresquiggle,))
+        doradoReturn = pool.apply_async(readDorado, args=(args.dorado,))
+        tomboReturn = pool.apply_async(readTombo, args=(args.tombo,))
+        controlReturn = pool.apply_async(generateControl, args=(args.basecalls,))
+
+        pool.close()
+        pool.join()
+
+        print("Done Reading,\tStart Evaluating...")
+
+        groundTruths = gtReturn.get()  #readChangepoints(args.changepoints)
+        # generating controls
+        randomBorders, uniformBorders = controlReturn.get()
 
         toolsResult = {
-            'dynamont' : readDynamont(args.dynamont),
-            'f5cEventalign' : readF5CEventalign(args.f5ceventalign, os.path.splitext(args.f5ceventalign)[0] + '.sum'),
-            'f5cResquiggle' : readF5CResquiggle(args.f5cresquiggle),
-            'dorado' : readDorado(args.dorado),
-            'tombo' : readTombo(args.tombo),
+            'dynamont' : dynamontReturn.get(), # readDynamont(args.dynamont),
+            'f5cEventalign' : f5ceventalignReturn.get(), # readF5CEventalign(args.f5ceventalign, os.path.splitext(args.f5ceventalign)[0] + '.sum'),
+            'f5cResquiggle' : f5cresquiggleReturn.get(), # readF5CResquiggle(args.f5cresquiggle),
+            'dorado' : doradoReturn.get(), # readDorado(args.dorado),
+            'tombo' : tomboReturn.get(), # readTombo(args.tombo),
+            'random' : randomBorders,
+            'uniform' : uniformBorders,
         }
 
         for tool in toolsResult:
             toNumpy(toolsResult[tool])
+        toNumpy(groundTruths)
 
         df = pd.DataFrame()
-        for tool, tool_results in toolsResult.items():
+        for tool, result in toolsResult.items():
             print(f"Evaluating {tool}...")
 
-            for maxDistance in range(0, 1000, 1):
+            for maxDistance in range(0, 50, 1):
                 print(f'Distance: {maxDistance}')
                 totalEdges = 0 # total edges in ground truth
                 foundEdges = 0 # found edges by tool
                 totalReads = 0 # total reads in ground truth
                 segmentedReads = 0 # segmented reads by tool
             
-                for i, (readid, gt_set) in enumerate(groundTruths.items()):
+                for i, (readid, gtSet) in enumerate(groundTruths.items()):
                     if (i+1) % 1000 == 0:
                         print(f'{i+1}/{len(groundTruths)}', end='\r')
-                    totalEdges += len(gt_set)
+                    totalEdges += len(gtSet)
                     totalReads += 1
 
-                    if readid in tool_results and tool_results[readid]:
+                    if readid in result and result[readid].size:
                         segmentedReads += 1
-                        foundEdges += evaluate(gt_set, tool_results, maxDistance)
+                        foundEdges += evaluate(gtSet, result[readid], maxDistance)
 
-                new_entry = pd.DataFrame({
+                newEntry = pd.DataFrame({
                     'tool': [tool],
                     'maxDistance': [maxDistance],
                     'foundEdges': [foundEdges],
@@ -165,7 +403,7 @@ def main() -> None:
                     'totalReads': [totalReads],
                     'segmentedReadsRatio': [segmentedReads / totalReads if totalReads > 0 else 0],
                 })
-                df = pd.concat((df, new_entry), ignore_index=True)
+                df = pd.concat((df, newEntry), ignore_index=True)
 
             print(f'Done: {i}/{len(groundTruths)}')
 
