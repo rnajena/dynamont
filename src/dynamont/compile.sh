e56987c83cfe73c5faf75209051fbfaa4869217b
Jannes Spangenberg <janspang@gmx.de>
2024-11-05 17:21:36 +0100

    update

--- src/dynamont/compile.sh
+++ src/dynamont/compile.sh
@@ -1,14 +1,6 @@
-echo "g++ -g -O3 -Wall -o dynamont_NTK dynamont_NTK.cpp utils.cpp"
-g++ -g -O3 -Wall -o dynamont_NTK dynamont_NTK.cpp utils.cpp
-echo "g++ -g -O3 -Wall -o dynamont_NT dynamont_NT.cpp utils.cpp"
-g++ -g -O3 -Wall -o dynamont_NT dynamont_NT.cpp utils.cpp
-echo "g++ -g -O3 -Wall -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp"
-g++ -g -O3 -Wall -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
-# echo "g++ -g -O3 -Wall -o dynamont_TK dynamont_TK.cpp utils.cpp"
-# g++ -g -O3 -Wall -o dynamont_TK dynamont_TK.cpp utils.cpp
-# echo "g++ -g -O3 -Wall -o segmentation_indel segmentation_indel.cpp utils.cpp"
-# g++ -g -O3 -Wall -o segmentation_indel segmentation_indel.cpp utils.cpp
-# echo "g++ -g -O3 -Wall -o segmentation_3d_sparsed_reduced segmentation_3d_sparsed_reduced.cpp utils.cpp"
-# g++ -g -O3 -Wall -o segmentation_3d_sparsed_reduced segmentation_3d_sparsed_reduced.cpp utils.cpp
-# echo "g++ -g -O3 -Wall -o segmentation_basic_sparsed_further segmentation_basic_sparsed_further.cpp utils.cpp"
-# g++ -g -O3 -Wall -o segmentation_basic_sparsed_further segmentation_basic_sparsed_further.cpp utils.cpp
+echo "NTK"
+g++ -std=c++20 -g -O3 -o dynamont_NTK dynamont_NTK.cpp utils.cpp
+echo "NT_banded"
+g++ -std=c++20 -g -O3 -o dynamont_NT dynamont_NT.cpp utils.cpp
+echo "NT"
+g++ -std=c++20 -g -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
