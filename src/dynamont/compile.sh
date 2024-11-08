54299020f434223d1c8843c3f2ef5fd8892d9db8
Jannes Spangenberg <janspang@gmx.de>
2024-11-08 21:25:37 +0100

    remove -g flag

--- src/dynamont/compile.sh
+++ src/dynamont/compile.sh
@@ -1,6 +1,6 @@
 echo "NTK"
-g++ -std=c++20 -g -O3 -o dynamont_NTK dynamont_NTK.cpp utils.cpp
+g++ -std=c++20 -O3 -o dynamont_NTK dynamont_NTK.cpp utils.cpp
 echo "NT_banded"
-g++ -std=c++20 -g -O3 -o dynamont_NT dynamont_NT.cpp utils.cpp
+g++ -std=c++20 -O3 -o dynamont_NT dynamont_NT.cpp utils.cpp
 echo "NT"
-g++ -std=c++20 -g -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
+g++ -std=c++20 -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
