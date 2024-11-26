16c0c506f6955534a6958383d66fdb3b09734f6d
Jannes Spangenberg <janspang@gmx.de>
2024-11-26 13:32:14 +0100

    move compile.sh

--- src/dynamont/compile.sh
+++
@@ -1,6 +0,0 @@
-echo "NTK"
-g++ -O3 -Wall -o dynamont_NTK dynamont_NTK.cpp utils.cpp
-echo "NT"
-g++ -O3 -Wall -o dynamont_NT dynamont_NT.cpp utils.cpp
-#echo "NT_banded"
-#g++ -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
