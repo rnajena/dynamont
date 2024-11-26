a87a0bf1f4cb48b5896ca7e5dfcfe7560da2b6db
Jannes Spangenberg <janspang@gmx.de>
2024-11-26 11:02:58 +0100

    update

--- src/dynamont/compile.sh
+++ src/dynamont/compile.sh
@@ -1,6 +1,6 @@
 echo "NTK"
-g++ -O3 -o dynamont_NTK dynamont_NTK.cpp utils.cpp
-echo "NT_banded"
-g++ -O3 -o dynamont_NT dynamont_NT.cpp utils.cpp
+g++ -O3 -Wall -o dynamont_NTK dynamont_NTK.cpp utils.cpp
 echo "NT"
-g++ -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
+g++ -O3 -Wall -o dynamont_NT dynamont_NT.cpp utils.cpp
+#echo "NT_banded"
+#g++ -O3 -o dynamont_NT_banded dynamont_NT_banded.cpp utils.cpp
