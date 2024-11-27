49b9232fed0f563562fdae294f19a27b8462d790
Jannes Spangenberg <janspang@gmx.de>
2024-11-27 22:10:07 +0100

    remove redundant file

--- compile.sh
+++
@@ -1,4 +0,0 @@
-echo "NTK"
-g++ -I include/ -O3 -Wall -o src/dynamont/dynamont_NTC src/dynamont/dynamont_NTC.cpp src/dynamont/utils.cpp
-echo "NT"
-g++ -I include/ -O3 -Wall -o src/dynamont/dynamont_NT src/dynamont/dynamont_NT.cpp src/dynamont/utils.cpp
