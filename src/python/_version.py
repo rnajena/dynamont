ea0b1643c12e46e4fb3a67b0760f039e3938fb79
Jannes Spangenberg <janspang@gmx.de>
2024-11-27 22:10:11 +0100

    update

--- src/python/_version.py
+++ src/python/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev289+g0573b17.d20241127'
-__version_tuple__ = version_tuple = (0, 1, 'dev289', 'g0573b17.d20241127')
+__version__ = version = '0.1.dev291+g766a5e4.d20241127'
+__version_tuple__ = version_tuple = (0, 1, 'dev291', 'g766a5e4.d20241127')
