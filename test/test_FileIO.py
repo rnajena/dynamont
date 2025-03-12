fbcb4eee397c93cbb82b5c31b58fb7e6630892b9
Jannes Spangenberg <janspang@gmx.de>
2025-03-12 15:03:10 +0100

    updated tests

--- test/test_FileIO.py
+++ test/test_FileIO.py
@@ -11,7 +11,7 @@ class TestSum(unittest.TestCase):
         signal = np.array([1.23456, 2.34567, 3.45678])
         read = "test_read"
         result = feedPipe(signal, read, mockPipe)
-        expectedCookie = "1.2345600,2.3456700,3.4567800\ntest_read\n"
+        expectedCookie = "1.23456,2.34567,3.45678\ntest_read\n"
         # Assert the communicate method was called with the expected input
         mockPipe.communicate.assert_called_once_with(input=expectedCookie)
         # Use self.assertEqual for consistency with unittest
@@ -29,7 +29,7 @@ class TestSum(unittest.TestCase):
             [2, 871, 8, 'A', 'GTACG', 'M', 1.0, 'NA'],
             [871, 1000, 7, 'T', 'CGTAC', 'M', 0.895648, 'NA']
         ], dtype=object)
-        result = formatSegmentationOutput(output, sigOffset, lastIndex, read)
+        result = formatSegmentationOutput(output, sigOffset, lastIndex, read, 5)
         assert np.array_equal(result, expected)
         
     def testFormtSegmentation(self):
