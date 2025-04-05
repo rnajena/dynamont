d4ee9874287ad3f4f4edcbe0010659d52917e875
Jannes Spangenberg <janspang@gmx.de>
2025-04-05 17:08:00 +0200

    fixing github action

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 VERSION=$(hatch version)  # Extract the short version string
 export PROJECT_VERSION=$VERSION  # Set environment variable
 
-mkdir -p build
-cd build
+mkdir -p _build
+cd _build
 # cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_BUILD_TYPE=Release
 cmake .. -DPROJECT_VERSION="$PROJECT_VERSION" -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
@@ -13,8 +13,8 @@ make install
 cd ..
 
 mkdir -p src/bin
-cp build/bin/dynamont-NTC src/bin/
-cp build/bin/dynamont-NT-banded src/bin/
+cp _build/bin/dynamont-NTC src/bin/
+cp _build/bin/dynamont-NT-banded src/bin/
 
 # python -m pip install --user --upgrade setuptools wheel build --ignore-installed -v .
 python -m build
