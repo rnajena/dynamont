f26ffea498de24b0e02bd2e870a843837a0fd8bf
Jannes Spangenberg <janspang@gmx.de>
2025-04-05 12:07:00 +0200

    add python wrapper to call cpp binaries

---
+++ src/python/nt_entry.py
@@ -0,0 +1,10 @@
+from os.path import join, dirname
+from subprocess import call
+from sys import exit, argv
+
+def main():
+    exe_path = join(dirname(__file__), "../bin/dynamont-NT-banded")
+    exit(call([exe_path] + argv[1:]))
+
+if __name__ == "__main__":
+    main()
