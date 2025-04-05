dde4b9a60372ac5b6f28ab837ba4189d32e74e78
Jannes Spangenberg <janspang@gmx.de>
2025-04-05 12:07:33 +0200

    update build scripts - add entry points to python wrappers for cpp binaries - update cpp version string dynamically

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+# Get the version from Hatch (Python)
+VERSION=$(hatch version)  # Extract the short version string
+export PROJECT_VERSION=$VERSION  # Set environment variable
+
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_BUILD_TYPE=Release
+# cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_BUILD_TYPE=Release
+cmake .. -DPROJECT_VERSION="$PROJECT_VERSION" -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
 cd ..
+
+mkdir -p src/bin
+cp build/bin/dynamont-NTC src/bin/
+cp build/bin/dynamont-NT-banded src/bin/
+
 # python -m pip install --user --upgrade setuptools wheel build --ignore-installed -v .
 python -m build
-twine upload dist/*
+# twine upload dist/*
