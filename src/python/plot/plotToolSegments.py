9e5321b25eff9451f4a52344cb9d44990260d452
Jannes Spangenberg <janspang@gmx.de>
2025-05-13 17:11:02 +0200

    bug fixes

--- src/python/plot/plotToolSegments.py
+++ src/python/plot/plotToolSegments.py
@@ -6,6 +6,8 @@
 
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 import read5_ont as r5
 from os.path import splitext, join
 import numpy as np
@@ -39,15 +41,14 @@ def readDynamont(file: str, readid : str) -> list:
                 continue
             try:
                 _, _, start, end, _, base, motif, *_ = line.strip().split(',')
+                segments.append((int(start), int(end), base, motif))
             except ValueError: # empty line in file
                 pass
-            segments.append((int(start), int(end), base, motif))
     return segments
 
-def readUncalled4(file : str, readid : str, kmerSize : int = 5) -> list:
+def readUncalled4(file : str, readid : str) -> list:
     print("Reading Uncalled4 output from " + file)
     segments = []
-    HALFKMERSIZE = kmerSize // 2
     with open(file, 'r') as f:
         next(f) # skip header
         # seq.name        seq.pos seq.strand      aln.id  seq.kmer        aln.read_id     dtw.start       dtw.length      dtw.current     dtw.current_sd
@@ -57,7 +58,7 @@ def readUncalled4(file : str, readid : str, kmerSize : int = 5) -> list:
                 continue
             if start == "*":
                 continue
-            segments.append((int(start), int(start) + int(length), kmer[HALFKMERSIZE], kmer))
+            segments.append((int(start), int(start) + int(length), kmer[len(kmer) // 2], kmer))
     return segments
 
 def readF5CResquiggle(file : str, readid : str, read : str, kmerSize : int = 5) -> list:
@@ -73,13 +74,12 @@ def readF5CResquiggle(file : str, readid : str, read : str, kmerSize : int = 5) 
             _, kmer_idx, start, end = line.strip().split('\t')
             if start == '.' or end == '.':
                 continue
-            segments.append((int(start), int(end), read[int(kmer_idx) + HALFKMERSIZE], read[int(kmer_idx) + kmerSize]))
+            segments.append((int(start), int(end), read[int(kmer_idx) + HALFKMERSIZE], read[int(kmer_idx): int(kmer_idx) + kmerSize]))
     return segments
 
-def readF5CEventalign(file: str, summary : str, readid : str, kmerSize : int = 5) -> list:
+def readF5CEventalign(file: str, summary : str, readid : str) -> list:
     print("Reading f5c Eventalign output from " + file)
     readNum = None
-    HALFKMERSIZE = kmerSize // 2
     with open(summary, 'r') as f:
         # read_index    read_name   fast5_path  model_name  strand  num_events  num_steps   num_skips   num_stays   total_duration  shift   scale   drift   var
         for line in f:
@@ -94,10 +94,10 @@ def readF5CEventalign(file: str, summary : str, readid : str, kmerSize : int = 5
         next(f) # skip header
         # contig    position    reference_kmer  read_index  strand  event_index event_level_mean    event_stdv  event_length    model_kmer  model_mean  model_stdv  standardized_level  start_idx   end_idx
         for line in f:
-            _, _, _, rNum, _, _, _, _, _, modelKmer, _, _, _, start, end = line.strip().split('\t')
+            _, _, _, rNum, _, _, _, _, _, kmer, _, _, _, start, end = line.strip().split('\t')
             if not rNum == readNum:
                 continue
-            segments.append((int(start), int(end), modelKmer[HALFKMERSIZE], modelKmer))
+            segments.append((int(start), int(end), kmer[len(kmer) // 2], kmer))
     return segments
 
 def readDorado(file : str, readid : str) -> list:
@@ -113,28 +113,22 @@ def readDorado(file : str, readid : str) -> list:
             segments.append((int(start), int(end), base, motif))
     return segments
 
-def readBasecalls(file : str, readid : str) -> str:
-    import pysam
-    with pysam.AlignmentFile(file, "rb", check_sq=False) as bamfile:
+def readBasecalls(file: str, readid: str) -> str:
+    from pysam import AlignmentFile
+    with AlignmentFile(file, "rb", check_sq=False) as bamfile:
         for read in bamfile.fetch(until_eof=True):
-            if not readid == read.query_name:
-                continue
-            return read.query_sequence
+            if readid == read.query_name:
+                return read.query_sequence
+    return None
 
 def main() -> None:
     basecolors = {
-    'A':'#377eb8',
-    'a':'#377eb8',
-    'C':'#ff7f00',
-    'c':'#ff7f00',
-    'G':'#4daf4a',
-    'g':'#4daf4a',
-    'T':'#f781bf',
-    't':'#f781bf',
-    'U':'#f781bf',
-    'u':'#f781bf',
-    'N':'#ffffff',
-    'n':'#ffffff',
+    'A':'#377eb8', 'a':'#377eb8',
+    'C':'#ff7f00', 'c':'#ff7f00',
+    'G':'#4daf4a', 'g':'#4daf4a',
+    'T':'#f781bf', 't':'#f781bf',
+    'U':'#f781bf', 'u':'#f781bf',
+    'N':'#ffffff', 'n':'#ffffff',
     }
     
     args = parse()
@@ -144,7 +138,7 @@ def main() -> None:
     tools = {
         "Dynamont" : np.array(readDynamont(args.dynamont, args.readid)),
         "Uncalled4" : np.array(readUncalled4(args.uncalled4, args.readid)),
-        "f5c Resquiggle" : np.array(readF5CResquiggle(args.f5cresquiggle, args.readid, read, args.k // 2)),
+        "f5c Resquiggle" : np.array(readF5CResquiggle(args.f5cresquiggle, args.readid, read, args.k)),
         "f5c Eventalign" : np.array(readF5CEventalign(args.f5ceventalign, splitext(args.f5ceventalign)[0] + '.sum', args.readid)),
         # "Tombo" : readTombo(args.tombo, args.readid),
         "Dorado" : np.array(readDorado(args.dorado, args.readid)),
@@ -159,20 +153,16 @@ def main() -> None:
     fig.supxlabel("Rel. Time (Sequencing Data Points)")
 
     for i, (tool, segments) in enumerate(tools.items()):
-        segments = np.unique(segments, axis=1)
-        # segments = segments[np.argsort(segments[:, 0].astype(int))]
+        segments = np.unique(segments, axis=0)
         ax[i].plot(signal, linewidth=1.0, c='black')
         ax[i].set_xlim((0, len(signal)))
-      #   ax[i].vlines(segments[:, 0], ymin=min(signal), ymax=max(signal), linewidth=0.5, linestyle='--', colors='red')
         ax[i].set_title(tool)
-        # ax[i].legend()
         ax[i].set_xticks(np.arange(0, len(signal), 1000))
 
         for s in segments:
             start = int(s[0])
             end = int(s[1])
             base = str(s[2])
-            # ax[i].text(start + (end - start)/2 - 6, -3.5, base, fontdict={'size' : 6, 'color':'black'}) # rotation=90
             ax[i].vlines([start, end], ymin=min(signal), ymax=max(signal), colors=basecolors[base], linestyles='--', linewidth=0.7)
             ax[i].add_patch(Rectangle((start, min(signal)), end - start, max(signal)-min(signal), alpha=0.4, edgecolor=basecolors[base], facecolor=basecolors[base]))
 
@@ -182,32 +172,35 @@ def main() -> None:
     plt.savefig(join(outpath, args.readid + "_tool_segmentation.png"), dpi=300)
     plt.close()
 
-    # fig, ax = plt.subplots(nrows = 5, figsize=(20,10), dpi=300)
-    # fig.suptitle(f"Segmentation of {args.readid} by Different Tools", fontsize=20)
-    # fig.supylabel("Current (pA)", fontsize=18)
-    # fig.supxlabel("Rel. Time (Sequencing Data Points)", fontsize=18)
+    print("Plotted: " + args.readid + "_tool_segmentation.svg")
 
-    # for i, (tool, segments) in enumerate(tools.items()):
-    #     segments = np.unique(segments, axis=1)
-    #     # segments = segments[np.argsort(segments[:, 0].astype(int))]
-    #     ax[i].plot(signal, linewidth=1.0, c='black')
-    #   #   ax[i].vlines(segments[:, 0], ymin=min(signal), ymax=max(signal), linewidth=0.5, linestyle='--', colors='red')
-    #     ax[i].set_title(tool, fontsize=18)
-    #     # ax[i].legend()
+    fig, ax = plt.subplots(nrows = 5, figsize=(10,10), dpi=300)
+    fig.suptitle(f"Segmentation of {args.readid} by Different Tools", fontsize=20)
+    fig.supylabel("Current (pA)", fontsize=18)
+    fig.supxlabel("Rel. Time (Sequencing Data Points)", fontsize=18)
 
-    #     for s in segments:
-    #         start = int(s[0])
-    #         end = int(s[1])
-    #         base = str(s[2])
-    #         # ax[i].text(start + (end - start)/2 - 6, -3.5, base, fontdict={'size' : 6, 'color':'black'}) # rotation=90
-    #         ax[i].vlines([start], ymin=min(signal), ymax=max(signal), colors=basecolors[base], linestyles='--', linewidth=0.7)
-    #         ax[i].add_patch(Rectangle((start, min(signal)), end - start, max(signal)-min(signal), alpha=0.4, edgecolor=basecolors[base], facecolor=basecolors[base]))
+    for i, (tool, segments) in enumerate(tools.items()):
+        segments = np.unique(segments, axis=0)
+        ax[i].plot(signal, linewidth=1.0, c='black')
+        ax[i].set_title(tool, fontsize=18)
 
-    #     ax[i].set_xticks(np.arange(0, len(signal), 500))
-    #     ax[i].set_xlim((19000, 22000))
+        for s in segments:
+            start = int(s[0])
+            end = int(s[1])
+            base = str(s[2])
+            ax[i].vlines([start], ymin=min(signal), ymax=max(signal), colors=basecolors[base], linestyles='--', linewidth=0.7)
+            ax[i].add_patch(Rectangle((start, min(signal)), end - start, max(signal)-min(signal), alpha=0.4, edgecolor=basecolors[base], facecolor=basecolors[base]))
 
-    # plt.tight_layout()
-    # plt.savefig(join(outpath, args.readid + "_tool_segmentation_region.svg"), dpi=300)
+        ax[i].set_xticks(np.arange(0, len(signal), 500))
+        # ax[i].set_xlim((19000, 22000)) #! for c28
+        ax[i].set_xlim((7400, 8000))
+
+    plt.tight_layout()
+    plt.savefig(join(outpath, args.readid + "_tool_segmentation_region.svg"), dpi=300)
+    plt.savefig(join(outpath, args.readid + "_tool_segmentation_region.pdf"), dpi=300)
+    plt.savefig(join(outpath, args.readid + "_tool_segmentation_region.png"), dpi=300)
+
+    print("Plotted: " + args.readid + "_tool_segmentation_region.svg")
 
 if __name__ == '__main__':
     main()
