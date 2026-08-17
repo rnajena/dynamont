354decffeb91a06cbd95614193d2c98d2852f2d3
JannesSP <jannes.spangenberg@gmail.com>
2026-08-17 16:35:02 +0200

    fix build information

--- build.sh
+++ build.sh
@@ -2,63 +2,39 @@
 set -euo pipefail
 
 BUILD_DIR="build"
-BIN_DIR="src/bin"
+PYTHON="python3"
 
 if [[ "${1:-}" == "--clean" ]]; then
     echo "[INFO] Cleaning build..."
-    rm -rf "${BUILD_DIR}" "${BIN_DIR}"
+    rm -rf "${BUILD_DIR}"
 fi
 
-mkdir -p "${BIN_DIR}"
-
-for cmd in cmake hatch python; do
-    if ! command -v "${cmd}" >/dev/null; then
-        echo "[ERROR] ${cmd} not found"
-        exit 1
-    fi
-done
+PYBIND11_DIR=$("${PYTHON}" -c 'import pybind11; print(pybind11.get_cmake_dir())')
 
 echo "[INFO] Getting project version..."
-PROJECT_VERSION=$(hatch version)
-echo " -> Version: ${PROJECT_VERSION}"
-export PROJECT_VERSION
+PROJECT_VERSION=$("${PYTHON}" -c 'from scikit_build_core.metadata.setuptools_scm import dynamic_metadata; print(dynamic_metadata("version"))')
 
-if command -v nproc >/dev/null; then
-    JOBS=$(nproc)
-elif command -v sysctl >/dev/null; then
-    JOBS=$(sysctl -n hw.ncpu)
-else
-    JOBS=1
-fi
+echo " -> Version: ${PROJECT_VERSION}"
+
+JOBS=$(nproc)
 
 echo "[INFO] Configuring CMake..."
 
 cmake -S . \
-      -B "${BUILD_DIR}" \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}" \
-      -DPROJECT_VERSION="${PROJECT_VERSION}" \
-      -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc"
+    -B "${BUILD_DIR}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}" \
+    -DBUILD_PYTHON_BINDING=ON \
+    -Dpybind11_DIR="${PYBIND11_DIR}" \
+    -DPROJECT_VERSION="${PROJECT_VERSION}" \
+    -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++ -static-libgcc"
 
 
 echo "[INFO] Building..."
 cmake --build "${BUILD_DIR}" --parallel "${JOBS}"
-echo "[INFO] Installing binaries..."
+echo "[INFO] Installing CMake targets..."
 cmake --install "${BUILD_DIR}"
-echo "[INFO] Copying binaries..."
-
-cp "${BUILD_DIR}/bin/dynamont-NTC" \
-   "${BUILD_DIR}/bin/dynamont-NT-banded" \
-   "${BUILD_DIR}/bin/dynamont-NT" \
-   "${BIN_DIR}/"
-
-echo "[INFO] Stripping binaries..."
-
-strip \
-    "${BIN_DIR}/dynamont-NTC" \
-    "${BIN_DIR}/dynamont-NT-banded" \
-    "${BIN_DIR}/dynamont-NT"
 
 echo "[INFO] Building Python package..."
-python -m build
+"${PYTHON}" -m build
 echo "[INFO] Build completed successfully."
